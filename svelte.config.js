c2ea5c9bfc3913b5890e25981dd603d42aaa2687
Ross Root <git@rossroot.com>
2026-07-25 16:03:16 -0700

    fix config some more

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,10 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+	},
 	kit: {
 		adapter: adapter({
 			pages: 'build',

cf5707553623af7f22f96303c6fc7bbde4e483ed
Ross Root <git@rossroot.com>
2026-07-25 15:57:25 -0700

    remove links to demo page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	import { resolve } from '$app/paths';
-</script>
-
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
@@ -21,10 +17,5 @@
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href={resolve('/sverdle')}>Sverdle</a> page illustrates SvelteKit's data loading and form handling.
-		Try using it with JavaScript disabled!
 	</p>
 </div>
